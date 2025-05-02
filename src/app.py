0e79677b029a33f959e8de6d51e56e526b628929
avadhoot05 <khedekaravadhoot@gmail.com>
2025-05-02 19:07:25 +0200

    create post endpoint to collect order data

---
+++ src/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+print("##########3")
+
+
+
+@app.route('/orders', methods=['POST'])
+def createOrder():
+    orderData = request.get_json()
+    print("XXXXXX")
+    if not orderData:
+        return jsonify({'error': 'No data provided'}), 400
+
+    # Log the received data
+    print("Received Order:", orderData)
+    orderId = orderData.get('orderId')
+    productName = orderData.get('productName')
+    quantity = orderData.get('quantity')
+    price = orderData.get('price')
+
+   
+
+    return jsonify({
+        'message': 'Order received',
+        'order': {
+            'orderId': orderId,
+            'productName': productName,
+            'quantity': quantity,
+            'price': price
+        }
+    }), 201
+
+# @app.route('/', methods=['GET'])
+# def homePage():
+#     return jsonify({
+#             'message': 'This is Home page',
+#         }), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
